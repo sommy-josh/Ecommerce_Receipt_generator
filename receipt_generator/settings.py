0da844b8d8506f994b4fb6dc16f2f98eaf980b92
sommy-josh <chisomzzy1@gmail.com>
2026-02-09 04:21:42 -0800

    added allowed host

--- receipt_generator/settings.py
+++ receipt_generator/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = os.environ.get('DEBUG') == 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST')
 
 
 # Application definition
