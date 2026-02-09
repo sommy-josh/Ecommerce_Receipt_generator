fd1c5b0f992fa3c0b5a9fa7c06ec7c1ad1b1ba76
sommy-josh <chisomzzy1@gmail.com>
2026-02-09 04:46:43 -0800

    corrected allowed host

--- receipt_generator/settings.py
+++ receipt_generator/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'False'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST')
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
